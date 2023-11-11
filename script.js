9edca97c46d6e641c1bfd30773bd3a50b4564311
Your Name <you@example.com>
2023-11-11 11:45:28 +0300

    v 3

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = 57;
+console.log (a);
+// let title=document.querySelector(".home__title")
+// title.textContent = "Привет из JS!"
+
+let b = document.querySelector(".btn")
+
+b.addEventListener('click',function(){
+    alert('Сталкер');
+})
