17f78c77f4bffcef7d6d05830c9d93254b5946d8
Your Name <you@example.com>
2023-11-25 11:46:51 +0300

    carousel

--- script.js
+++ script.js
@@ -3,16 +3,43 @@ console.log (a);
 // let title=document.querySelector(".home__title")
 // title.textContent = "Привет из JS!"
 
-let b = document.querySelector(".btn")
 
-b.addEventListener('click',function(){
-    alert('Сталкер');
-})
-
-
-let burger = document.querySelector('.burger-menu')
-let nav = document.querySelector('.main.nav')
-burger.addEventListener('click' ,function(){
-    burger.classList.toggle('active')
-    nav.classList.toggle('active')
-})
+// let burger = document.querySelector('.burger-menu')
+// let nav = document.querySelector('.main.nav')
+// burger.addEventListener('click' ,function(){
+//     burger.classList.toggle('active')
+//     nav.classList.toggle('active')
+// })
+$('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+//   $('.carousel').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true
+//   });
+                  
